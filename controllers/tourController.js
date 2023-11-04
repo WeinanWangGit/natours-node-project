962e132f9e3525b0fea71de3871e64f91f995738
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-04 16:40:28 +0000

    Setting up ESLint + Prettier in vs code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 exports.checkID = (req, res, next, val) => {
@@ -56,19 +56,20 @@ exports.createTour = (req, res) => {
 
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
+  // const newTour = { id: newId, ...req.body };
 
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    () => {
       res.status(201).json({
         status: 'success',
         data: {
           newTour,
         },
       });
-    }
+    },
   );
 };
 
