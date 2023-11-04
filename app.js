b11d45522ee0946bb86193190bd4cc6aa70560d8
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-04 11:05:15 +0000

    Environment Variables  running different mode

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -22,7 +25,6 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
