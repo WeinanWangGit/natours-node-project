f668253d67ad00ee900815d1e02027fd53a88e4e
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-17 16:03:45 +0000

    Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,18 +2,29 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-
-    const query = Tour.find(queryObj);
 
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // { duration: { $gte: '5' }, difficulty: 'easy' }
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+    // gte, gt, lte, lt
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
