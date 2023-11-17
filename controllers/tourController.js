a83ce19d123514221364374768d83758952d0efd
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-17 16:23:06 +0000

    Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,6 +36,15 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createAt');
     }
 
+    // 3) Filed Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+      // -means NOT included
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
