52be9422a34786e56c48552e9cdda1b5ebba8fd1
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-17 16:14:57 +0000

    Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,7 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
@@ -15,7 +15,7 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equals('easy');
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
@@ -24,7 +24,17 @@ exports.getAllTours = async (req, res) => {
     // { duration: { gte: '5' }, difficulty: 'easy' }
     // gte, gt, lte, lt
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+      // sort('price ratingsAverage') -price
+    } else {
+      query = query.sort('-createAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
