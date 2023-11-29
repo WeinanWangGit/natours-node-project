f7aba164e1ecbad24ea472db8d6c671e2ab661d3
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-11-29 10:29:58 +0000

    Password Reset Functionality: Reset Token

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -38,6 +39,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordRestToken: String,
+  passwordRestExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -69,6 +72,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   //   False means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordRestToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordRestToken);
+
+  this.passwordRestExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
