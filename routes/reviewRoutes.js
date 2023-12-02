5b2fcc6516e1feef3e941d3637a28f9925b45f56
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-12-02 07:46:12 +0000

    Adding Missing Authentication and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController.js');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 // POST /tour/23fdsd/reviews Nested Routes
 // GET /tour/23fdsd/reviews
 // POST /reviews
@@ -12,7 +14,6 @@ router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -21,7 +22,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReviews)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;
