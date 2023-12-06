2e20afddf7e4df878674cc6c95c65c1934390a36
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-12-06 12:09:27 +0000

    Fix all SSR setting, map replacing, user login, updating, reset password

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorhandler = require('./controllers/errorController');
@@ -24,7 +25,55 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
+
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://js.stripe.com',
+  'https://m.stripe.network',
+  'https://*.cloudflare.com',
+];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+];
+const connectSrcUrls = [
+  'https://unpkg.com',
+  'https://tile.openstreetmap.org',
+  'https://*.stripe.com',
+  'https://bundle.js:*',
+  'ws://127.0.0.1:*/',
+];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      scriptSrc: ["'self'", 'https:', 'http:', 'blob:', ...scriptSrcUrls],
+      frameSrc: ["'self'", 'https://js.stripe.com'],
+      objectSrc: ["'none'"],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:', 'https://m.stripe.network'],
+      childSrc: ["'self'", 'blob:'],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      formAction: ["'self'"],
+      connectSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        'data:',
+        'blob:',
+        ...connectSrcUrls,
+      ],
+      upgradeInsecureRequests: [],
+    },
+  }),
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -41,6 +90,8 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
